052ab10316aba61fedd4be4a4a2e216bc6bf5c9a
pallabojha <ojhapallab@gmail.com>
2017-02-25 08:32:09 +0530

    Add files via upload

---
+++ AssingmentProject.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assingmentproject;
+
+
+public class AssingmentProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=20, b=10,c,d,e;
+        b=a-- - --a;
+        c=a--;
+        d=a>>2;
+        e=a&b;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        
+    }
+    
+}
