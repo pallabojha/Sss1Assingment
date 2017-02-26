f77e5c0784a75d19e792aedb5abab8bce998d0b2
pallabojha <ojhapallab@gmail.com>
2017-02-26 08:35:38 +0530

    Add files via upload

---
+++ Sss1Inroduce.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sss1inroduce;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Sss1Inroduce {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=10, b=20;
+        int c=a|b;
+        System.out.println(c);
+    }
+    
+}
